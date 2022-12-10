4f7cd27fc266fa512c217ea84e950a453ac5ca21
ewomatc <iamgreatdiro@gmail.com>
2022-12-10 13:23:41 -0800

    print all single digits in base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Dscription: prints all single digit numbers in base 10 from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n')
+		return (0);
+}
