73aa1b1c0b39acdfa7555bdaa67eb28b09fe4aed
ewomatc <iamgreatdiro@gmail.com>
2022-12-10 13:44:09 -0800

    print lowercase letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Description: print lowercase letters reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 'z'; num >= 'a'; num--)
+	{
+		purchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
