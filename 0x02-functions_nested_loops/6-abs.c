0d17752dd3bc0e601e244f5e0ee820dfb62b0ab5
ewomatc <iamgreatdiro@gmail.com>
2022-12-13 13:39:44 -0800

    return absolute valu of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - complete absolute value of integers
+ * @t: integer to be completed
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
