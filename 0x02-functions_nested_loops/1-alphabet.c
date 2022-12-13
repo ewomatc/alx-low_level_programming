cb95a02bbe5c45e7b00d264cb6298cc49d1f302d
ewomatc <iamgreatdiro@gmail.com>
2022-12-13 12:46:06 -0800

    create 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase followed by newline
+ * print_alphabet - prints the alphabet followed by a new line
  */
-void print_aphabet(void)
+void print_alphabet(void)
 {
-	char letter;
+	char la;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
 
 	_putchar('\n');
-
-	return (0);
 }
